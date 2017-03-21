6acfa5d4e015eefba81e392b5d57ed5f751ae630
Mario Rosenthal <mario.f.rosenthal@gmail.com>
2017-03-21 15:40:30 -0400

    working api w/ db seed

--- app/assets/javascripts/components/event_application.js.jsx
+++ app/assets/javascripts/components/event_application.js.jsx
@@ -1,3 +1,21 @@
 var EventApplication = React.createClass({
-
-})
+  render: function() {
+    return(
+      <div className="container">
+        <div className="jumbotron">
+          <h1>
+            ReactJS
+          </h1>
+          <p>
+            blah blah
+          </p>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <EventTable />
+          </div>
+        </div>
+      </div>
+    )
+  }
+});
